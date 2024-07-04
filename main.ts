a91b2a94cfa18ab8b506f5f4b7d04ca64a62d8f6
ContainedBlargh <jon@zendata.dk>
2024-07-04 12:00:20 +0200

    Initial commit

---
+++ main.ts
@@ -0,0 +1,66 @@
+import Handlebars from "npm:handlebars";
+import { parseArgs } from "https://deno.land/std@0.221.0/cli/mod.ts";
+import { difference } from "https://deno.land/std@0.221.0/datetime/mod.ts";
+const encoder = new TextEncoder();
+
+async function exists(path: string) {
+    try {
+        await Deno.lstat(path);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+const helpers = {
+    "years-since": function(text: string) {
+        const date = new Date(text);
+        const diff = difference(date, new Date(), {
+            units: ["years"]
+        });
+        return diff.years;
+    }
+};
+
+async function main() {
+    const args = parseArgs(Deno.args.slice(1));
+
+    if (args['help'] || args['h'] || args['?'] || args['\\?']) {
+        await Deno.stderr.write(encoder.encode("TODO: Provide help."));
+        return;
+    }
+
+    const hbs = args['_'].find(it => `${it}`.endsWith(".hbs"));
+    const json = args['_'].find(it => `${it}`.endsWith(".json"));
+
+    let o = args['o'] || args['output'];
+    if (!hbs || !(await exists(hbs as string))) {
+        Deno.stderr.writeSync(encoder.encode("ERROR: Please provide a single .hbs template file.\n"));
+        return;
+    }
+    if (!json || !(await exists(json as string))) {
+        Deno.stderr.writeSync(encoder.encode("ERROR: Please provide a single .json data file.\n"));
+        return;
+    }
+    if (!o) {
+        o = (hbs as string).replace(/\.hbs$/, '');
+        Deno.stderr.writeSync(encoder.encode(`WARNING: No output path set, using default path: ${o}.\n`));
+    }
+    
+
+    for (const [key, func] of Object.entries(helpers)) {
+        Handlebars.registerHelper(key, func);
+    }
+
+    let raw = await Deno.readTextFile(json as string);
+    if ((o as string).endsWith(".tex")) {
+        raw = raw.replaceAll("#", "\\\\#").replaceAll("&", "\\\\&");
+    }
+    const data = JSON.parse(raw);
+    const content = await Deno.readTextFile(hbs as string);
+    const template = Handlebars.compile(content);
+    const output = template(data);
+    await Deno.writeTextFile(o as string, output);
+}
+
+await main();
